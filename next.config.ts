5ae7ea278aec13198d4dbfb49228f1a29bc69596
praveen-kaia <praveen.challa@kaia.io>
2025-06-09 16:05:46 +0800

    updated cors

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/signAsFeePayer",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "false" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "POST",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "Content-Length, Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
